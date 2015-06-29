fc9ae5e95d16816a19ee7c8c8cda9baa73f3025e
mony adey <madey@skoruz.com>
2015-06-29 16:30:43 -0700

    testing

---
+++ Box.java
@@ -0,0 +1,38 @@
+package Day10;
+
+public class Box {
+	
+	
+	private int width;
+	private int depth;
+	private int height;
+	
+	Box(){
+		
+		this(10,2,3);
+		width = 10;
+		depth = 10;
+		height = 10;
+		System.out.println("In default constructor of box class");
+	}
+	
+	Box(int width, int depth, int height){
+		
+		//this();
+		diplayData();
+		System.out.println("In parameterized constructor");
+		this.width = width;
+		this.depth = depth;
+		this.height = height;
+		diplayData();
+		
+	}
+		
+	public void diplayData(){
+		
+		System.out.println("width: "+width+" depth: "+depth+" height: "+height);
+	}
+	
+	
+
+}
